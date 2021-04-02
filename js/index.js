3af9667da68bba996c2eb52a20b8356fe0c411d7
Freddy <freddy12gutierrez@gmail.com>
2021-04-02 11:28:12 -0600

    loader y mas agregado

--- js/index.js
+++ js/index.js
@@ -5,13 +5,15 @@ window.addEventListener('scroll', function(){
 });
 
 //TYPED INICIACION
-var typed3 = new Typed('#typed3', {
-    strings: ['Desarrollador Front-End', 'Programador Jr', 'Diseñador Web'],
-    typeSpeed: 75,
-    backSpeed: 50,
-    smartBackspace: true,
-    loop: true,
-  });
+window.addEventListener('load', function(){
+    var typed3 = new Typed('#typed3', {
+        strings: ['Desarrollador Front-End', 'Programador Jr', 'Diseñador Web'],
+        typeSpeed: 75,
+        backSpeed: 50,
+        smartBackspace: true,
+        loop: true,
+      });
+});
 
 //NAVEGACION JAILOSA
 
@@ -28,7 +30,7 @@ boton.addEventListener("click",function(){
         enlaces.className = ('enlaces uno');
         contador = 0;
     }
-})
+});
 
 window.addEventListener('resize', function(){
     if(screen.width > 750){
@@ -37,43 +39,11 @@ window.addEventListener('resize', function(){
         enlaces.className = ('enlaces uno');
 
     }
-})
+});
 
-
-
-// window.addEventListener('scroll', function(){
-//     let head = document.getElementById('head').offsetTop;
-//     let info = document.getElementById('info').offsetTop;
-//     let proyectos = document.getElementById('proyectos').offsetTop;
-//     let contacto = document.getElementById('contacto').offsetTop;
-//     let ventana = window.pageYOffset;
-
-//     console.log(head,info,proyectos,contacto)
-//     console.log(ventana)
-
-//     if(ventana==head && ventana < info){
-//         document.getElementById('link-inicio').classList.add('active')
-//         document.getElementById('link-info').classList.remove('active')
-//         document.getElementById('link-proyectos').classList.remove('active')
-//         document.getElementById('link-contacto').classList.remove('active')
-//     }
-//     else if(ventana>=info && ventana < proyectos){
-//         document.getElementById('link-info').classList.add('active')
-//         document.getElementById('link-inicio').classList.remove('active')
-//         document.getElementById('link-proyectos').classList.remove('active')
-//         document.getElementById('link-contacto').classList.remove('active')
-//     }
-//     else if(ventana>=proyectos){
-//         document.getElementById('link-proyectos').classList.add('active')
-//         document.getElementById('link-inicio').classList.remove('active')
-//         document.getElementById('link-info').classList.remove('active')
-//         document.getElementById('link-contacto').classList.remove('active')
-//     }
-//     else if(ventana>proyectos){
-//         document.getElementById('link-contacto').classList.add('active')
-//         document.getElementById('link-inicio').classList.remove('active')
-//         document.getElementById('link-info').classList.remove('active')
-//         document.getElementById('link-proyectos').classList.remove('active')
-//     }
-
-// });
+//SPINNER
+let spinner = document.querySelector('#contenedor_carga');
+        window.addEventListener('load', function(){
+            spinner.style.visibility = 'hidden';
+            spinner.style.opacity = '0';
+        });
