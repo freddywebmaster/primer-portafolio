e10cb4d64d71332e2aa2e3317d3fd550c3260b00
freddy <freddy12gutierrez@gmail.com>
2021-05-05 04:23:57 -0600

    add new projects and new images

---
+++ js/api.js
@@ -0,0 +1,33 @@
+const url = "js/data.json";
+const rutaImg = 'img/ProyectosDestacados/';
+fetch(url)
+  .then((respuesta) => respuesta.json())
+  .then((datos) => {
+    let box = document.querySelector("#api-pro");
+    console.log(datos.projects);
+    for(let i=0; i<datos.projects.length; i++){
+        console.log(datos.projects[i].title);
+        box.innerHTML += `
+        <div class="carta">
+                    <div class="imagen">
+                        <img src="${rutaImg}${datos.projects[i].img}" alt="proyecto-freddy">
+                    </div>
+                    <div class="info-proyecto">
+                        <div class="title-proyect">
+                            <h3 class="title-w">${datos.projects[i].title}</h3>
+                            <i class="far fa-plus-square"></i>
+                        </div>
+                        <div class="fecha-proyecto">
+                            ${datos.projects[i].category} / ${datos.projects[i].date}
+                        </div>
+                        <div class="botones-proyectos">
+                            <a target="_blank" class="btn btn-secundario m-top" href="${datos.projects[i].visit}"><i
+                                    class="fas fa-globe-americas"></i> Visit</a>
+                            <a target="_blank" class="btn btn-tres m-top" href="${datos.projects[i].code}"><i
+                                    class="fas fa-code"></i> Source</a>
+                        </div>
+                    </div>
+                </div>
+        `
+    }
+  });
